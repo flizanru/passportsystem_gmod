007fff4c414e28c6181ba3d47768573db2ff6373
WinCode <92253313+WinCode2023@users.noreply.github.com>
2023-12-24 12:36:35 +0300

    Security Update 1.3
    Many requests have been screened and the system has become more secure. As well:
    
    - Added automatic nickname change (in the database) when changing.
    
    - Fixed a bug that when the server is turned off, the "wanted" statuses remain the same

--- flizan_roleplaysystems/lua/autorun/server/loader.lua
+++ flizan_roleplaysystems/lua/autorun/server/loader.lua
@@ -3,4 +3,27 @@ include("mysql/create_tables.lua")
 include("createpass/sv_pass.lua")
 include("createpass/sv_playernewjoin.lua")
 include("createpass/sv_playerupdate.lua")
-print("FL-System 1.2 has been successfully loaded")
+print("FL-System 1.3 has been successfully loaded")
+
+local currentVersion = "1.3" 
+
+local function initializeDatabase()
+    sql.Query("CREATE TABLE IF NOT EXISTS flizan_pass_version (version TEXT, first_load DATETIME)")
+
+    local data = sql.Query("SELECT * FROM flizan_pass_version")
+
+    if not data then
+        sql.Query("INSERT INTO flizan_pass_version (version, first_load) VALUES ('" .. currentVersion .. "', DATETIME('now'))")
+        print("[POLICE SYSTEM] The first launch! The version is set to " .. currentVersion)
+    else
+        local lastVersion = data[#data].version
+        if lastVersion ~= currentVersion then
+            sql.Query("INSERT INTO flizan_pass_version (version, first_load) VALUES ('" .. currentVersion .. "', DATETIME('now'))")
+            print("[POLICE SYSTEM] A new version has been discovered! Upgrade from " .. lastVersion .. " to " .. currentVersion)
+        else
+            print("[POLICE SYSTEM] Launching an existing version " .. currentVersion)
+        end
+    end
+end
+
+initializeDatabase()
--- flizan_roleplaysystems/lua/createpass/sv_playerupdate.lua
+++ flizan_roleplaysystems/lua/createpass/sv_playerupdate.lua
@@ -1,14 +1,48 @@
+hook.Add("CanChangeRPName", "UpdatePlayerNicknameInDB", function(ply, newName)
+    if IsValid(ply) then
+        local steamID = policeDb:escape(ply:SteamID())
+
+        local function updateNickname()
+            local escapedName = policeDb:escape(newName)
+            local query = policeDb:query("UPDATE flizan_player_info SET nickname = '" .. escapedName .. "' WHERE steamid = '" .. steamID .. "'")
+
+            query.onSuccess = function()
+                print("[POLICE SYSTEM] Nickname updated for player " .. ply:Nick())
+            end
+
+            query.onError = function(_, err)
+                print("[POLICE SYSTEM] Error updating nickname for player " .. ply:Nick() .. ": " .. err)
+            end
+
+            query:start()
+        end
+
+        if ply:Nick() == newName then
+            updateNickname()
+        else
+            timer.Simple(1, function()
+                if IsValid(ply) and ply:Nick() == newName then
+                    updateNickname()
+                end
+            end)
+        end
+    end
+end)
+
 hook.Add("playerWanted", "SetPlayerWanted", function(criminal, actor, reason)
     if IsValid(criminal) and IsValid(actor) then
-        local criminalSteamID = criminal:SteamID()
-        local actorSteamID = actor:SteamID()
+        local criminalSteamID = policeDb:escape(criminal:SteamID())
+        local actorSteamID = policeDb:escape(actor:SteamID())
+        local escapedActorName = policeDb:escape(actor:Nick())
+        local escapedCriminalName = policeDb:escape(criminal:Nick())
+        local escapedReason = policeDb:escape(reason)
 
         local query = policeDb:query("UPDATE flizan_player_info SET wanted = 1 WHERE steamid = '" .. criminalSteamID .. "'")
 
         query.onSuccess = function()
             criminal:SendLua([[chat.AddText(Color(30, 144, 255), "[Police Department] ", Color(255, 255, 255), "You are now wanted.")]])
 
-            local logQuery = policeDb:query("INSERT INTO flizan_logs_police (name, steamid, name2, steamid2, date, action, reason) VALUES ('" .. actor:Nick() .. "', '" .. actorSteamID .. "', '" .. criminal:Nick() .. "', '" .. criminalSteamID .. "', NOW(), 'wantedplayer', '" .. reason .. "')")
+            local logQuery = policeDb:query("INSERT INTO flizan_logs_police (name, steamid, name2, steamid2, date, action, reason) VALUES ('" .. escapedActorName .. "', '" .. actorSteamID .. "', '" .. escapedCriminalName .. "', '" .. criminalSteamID .. "', NOW(), 'wantedplayer', '" .. escapedReason .. "')")
 
             logQuery.onError = function(_, err)
                 print("[POLICE SYSTEM] Error writing log: " .. err)
@@ -27,8 +61,8 @@ end)
 
 hook.Add("playerUnWanted", "UnSetPlayerWanted", function(excriminal, actor)
     if IsValid(excriminal) and IsValid(actor) then
-        local excriminalSteamID = excriminal:SteamID()
-        local actorSteamID = actor:SteamID()
+        local excriminalSteamID = policeDb:escape(excriminal:SteamID())
+        local actorSteamID = policeDb:escape(actor:SteamID())
 
         local query = policeDb:query("UPDATE flizan_player_info SET wanted = 0 WHERE steamid = '" .. excriminalSteamID .. "'")
 
@@ -52,16 +86,31 @@ hook.Add("playerUnWanted", "UnSetPlayerWanted", function(excriminal, actor)
     end
 end)
 
+
 hook.Add("PlayerDisconnected", "ClearWantedOnDisconnect", function(ply)
-    local steamID = ply:SteamID()
+    local steamID = policeDb:escape(ply:SteamID())
     local query = policeDb:query("UPDATE flizan_player_info SET wanted = 0 WHERE steamid = '" .. steamID .. "'")
 
     query.onSuccess = function()
-     --   print("[POLICE SYSTEM] Player " .. ply:Nick() .. " disconnected. Wanted status reset.")
+        -- print("[POLICE SYSTEM] Player " .. ply:Nick() .. " disconnected. Wanted status reset.")
     end
 
     query.onError = function(_, err)
-       print("[POLICE SYSTEM] Error updating wanted status for player " .. ply:Nick() .. ": " .. err)
+        print("[POLICE SYSTEM] Error updating wanted status for player " .. ply:Nick() .. ": " .. err)
+    end
+
+    query:start()
+end)
+
+hook.Add("ShutDown", "ResetAllWantedStatuses", function()
+    local query = policeDb:query("UPDATE flizan_player_info SET wanted = 0 WHERE wanted = 1")
+
+    query.onSuccess = function()
+        print("[POLICE SYSTEM] All wanted statuses reset on shutdown.")
+    end
+
+    query.onError = function(_, err)
+        print("[POLICE SYSTEM] Error resetting wanted statuses on shutdown: " .. err)
     end
 
     query:start()
